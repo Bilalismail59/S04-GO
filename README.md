2a93ff0ee82dbb05e3ddfe55cbb27c83ef72ff9b
Ismail Bilal <ismobilal47@gmail.com>
2024-11-06 09:32:34 +0000

    correction

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # gotest
 Juste pour tester un environnement go
+
+## paquet principal
+
+importer  "journal"
+
+type  Joueur  struct {
+	pseudo  chaîne
+	âge     int
+	hp      int
+	mp      int
+	   chaîne de sexe
+	intel   int
+}
+
+func ( p  * Joueur ) setAge ( âge  int ) {
+	p . âge  =  âge
+}
+
+var (
+	joueur  =  créer ( carte [ chaîne ] Joueur )
+)
+
+fonction  principale () {
+	//var joueur1 Joueur
+	joueur2  :=  Joueur { pseudo : "saryon" , age : 15 , hp : 10 , mp : 10 , sexe : "" , intel : 12 }
+	joueur [ "saryon" ] =  joueur2
+	enregistrer . Printf ( "%#v" , joueur2 )
+	enregistrer . Printf ( "L'âge de %v : %v" , joueur2 . pseudo , joueur2 . age )
+	joueur2 . âge  =  16
+	enregistrer . Printf ( "L'âge de %v : %v" , joueur2 . pseudo , joueur2 . age )
+	joueur2 . setAge ( 17 )
+	enregistrer . Printf ( "L'âge de %v : %v" , joueur2 . pseudo , joueur2 . age )
+
+}
--- main.go
+++ main.go
@@ -1,34 +1,23 @@
 package main
 
-import "log"
-
-type Player struct {
-	pseudo string
-	age    int
-	hp     int
-	mp     int
-	sexe   string
-	intel  int
-}
-
-func (p *Player) setAge(age int) {
-	p.age = age
-
-}
-
-var (
-	player = make(map[string]Player)
-)
-
 func main() {
-	//var joueur1 Player
-	joueur2 := Player{pseudo: "Bilal", age: 15, hp: 10, mp: 10, sexe: "m", intel: 12}
-	player["saryon"] = joueur2
-	log.Printf("%#v", joueur2)
+	t := Task{
+		Title:       "Dormir",
+		Description: "C'est bien de dormir",
+		Completed:   false,
+	}
+	// Notre map de task stocke des pointers de task
+	// Il faut donc utiliser &t
+	tasks[t.Title] = &t
+	//log.Printf("Notre tâche : %#v", t)
+	//t.Display()
 
-	log.Printf("L'age de %v : %v", joueur2.pseudo, joueur2.age)
-	joueur2.age = 16
-	log.Printf("L'age de %v : %v", joueur2.pseudo, joueur2.age)
-	joueur2.setAge(17)
-	log.Printf("L'age de %v : %v", joueur2.pseudo, joueur2.age)
+	//displayAllTasks()
+
+	//addTask()
+	//t.Display()
+	t.Done()
+	//t.Display()
+	displayAllTasks()
+
 }
