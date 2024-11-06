3d23252355815d7ff9ebc49ed889a307908f1ff2
Ismail Bilal <ismobilal47@gmail.com>
2024-11-06 13:01:43 +0000

    correction

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
+import "log"
+
+var (
+	token = "c732a4f732342956ec521490b59a7dce"
+)
+
 func main() {
-	t := Task{
-		Title:       "Dormir",
-		Description: "C'est bien de dormir",
-		Completed:   false,
+	ville := "najac"
+
+	coord, err := getCoordFfromCity(ville)
+	if err != nil {
+		log.Fatalln(err)
+
 	}
-	// Notre map de task stocke des pointers de task
-	// Il faut donc utiliser &t
-	tasks[t.Title] = &t
-	//log.Printf("Notre tâche : %#v", t)
-	//t.Display()
-
-	//displayAllTasks()
-
-	//addTask()
-	//t.Display()
-	t.Done()
-	//t.Display()
-	displayAllTasks()
-
+	log.Printf("%#v", coord)
 }
